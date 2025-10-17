4c6a7d6d34ce58affc5adc34c8a82ae3021913af
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2025-10-17 11:44:08 +0300

    fix testing delete

--- src/testRouter/testRouter.ts
+++ src/testRouter/testRouter.ts
@@ -1,12 +1,18 @@
 import { Router, Response } from 'express'
 import { blogsDB, db, postsDB } from '../db/db'
 import { HttpStatus } from '../core/types/http-statuses'
+import { BlogsModel } from '../models/blogs.model'
+import { PostsModel } from '../models/posts.model'
 
 export const testingRouter = Router({})
 
-testingRouter.delete('/all-data', (_, res: Response) => {
+testingRouter.delete('/all-data', async (_, res: Response) => {
 	db.length = 0
 	blogsDB.length = 0
 	postsDB.length = 0
+
+	await BlogsModel.deleteMany({})
+	await PostsModel.deleteMany({})
+
 	res.sendStatus(HttpStatus.NoContent)
 })
