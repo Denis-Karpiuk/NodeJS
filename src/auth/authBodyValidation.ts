11a48b5d77b40cbd933365987e33fba32e9f3f40
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2025-10-29 13:51:03 +0300

    add validation body auth

---
+++ src/auth/authBodyValidation.ts
@@ -0,0 +1,16 @@
+import { body } from 'express-validator'
+
+export const authBodyValidator = [
+	body('loginOrEmail')
+		.trim()
+		.isString()
+		.withMessage('Поле login должно быть строкой'),
+
+	,
+	body('password')
+		.trim()
+		.isString()
+		.withMessage('Поле password должно быть строкой')
+		.isLength({ min: 2, max: 20 })
+		.withMessage('Максимальная длина password — 20 символов'),
+]
--- src/auth/router/auth.router.ts
+++ src/auth/router/auth.router.ts
@@ -1,15 +1,24 @@
-import { Router } from 'express'
+import { Request, Router, Response } from 'express'
 import { authService } from '../service/auth.service'
 import { HttpStatus } from '../../core/types/http-statuses'
+import { adminGuardMiddleware } from '../../core/middlewares/adminGuardMiddleware.middleware'
+import { validation } from '../../core/middlewares/validatation.middleware'
+import { authBodyValidator } from '../authBodyValidation'
 
-export const authRouter = Router({}).post('/login', async (req, res) => {
-	const result = await authService.login(req.body)
+export const authRouter = Router({}).post(
+	'/login',
+	adminGuardMiddleware,
+	authBodyValidator,
+	validation,
+	async (req: Request, res: Response) => {
+		const result = await authService.login(req.body)
 
-	if (!result.success) {
-		res.status(HttpStatus.Unauthorized).send({
-			errorMessages: [result.error],
-		})
+		if (!result.success) {
+			res.status(HttpStatus.Unauthorized).send({
+				errorMessages: [result.error],
+			})
+		}
+
+		res.status(HttpStatus.NotFound)
 	}
-
-	res.status(HttpStatus.NotFound)
-})
+)
