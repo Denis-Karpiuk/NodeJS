4d4fc7cd4bc1c2a40e3c65b2f71cd85f41377a17
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2025-11-13 13:38:54 +0300

    fix email transporter

--- src/core/adapters/emailAdapter.ts
+++ src/core/adapters/emailAdapter.ts
@@ -2,11 +2,13 @@ import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const transport = nodemailer.createTransport({
-	service: 'gmail',
+export const transport = nodemailer.createTransport({
+	host: process.env.SMTP_HOST!,
+	port: Number(process.env.SMTP_PORT),
+	secure: false,
 	auth: {
-		user: process.env.EMAIL,
-		pass: process.env.EMAIL_PASS,
+		user: process.env.SMTP_USER,
+		pass: process.env.SMTP_PASS,
 	},
 })
 
