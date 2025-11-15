038529a7939070c4f6fb973922f45630fdaeef81
Dzianis Karpiuk <dzianiskarpiuk@gmail.com>
2025-11-16 01:29:56 +0300

    change email transport

--- src/core/adapters/emailAdapter.ts
+++ src/core/adapters/emailAdapter.ts
@@ -1,38 +1,38 @@
 import { Resend } from 'resend'
-// import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
-// export const transport = nodemailer.createTransport({
-// 	host: process.env.SMTP_HOST!,
-// 	port: Number(process.env.SMTP_PORT),
-// 	secure: false,
-// 	auth: {
-// 		user: process.env.SMTP_USER,
-// 		pass: process.env.SMTP_PASS,
-// 	},
-// })
-
-// export const emailAdapter = {
-// 	sendEmail: async (
-// 		emailAddress: string,
-// 		subject: string,
-// 		template: string
-// 	): Promise<boolean> => {
-// 		const info = await transport.sendMail({
-// 			from: 'Denis <' + process.env.EMAIL + '>',
-// 			to: emailAddress,
-// 			subject: subject,
-// 			html: template,
-// 		})
-
-// 		return !!info
-// 	},
-// }
+export const transport = nodemailer.createTransport({
+	host: process.env.SMTP_HOST!,
+	port: Number(process.env.SMTP_PORT),
+	secure: false,
+	auth: {
+		user: process.env.SMTP_USER,
+		pass: process.env.SMTP_PASS,
+	},
+})
 
 export const emailAdapter = {
+	sendEmail: async (
+		emailAddress: string,
+		subject: string,
+		template: string
+	): Promise<boolean> => {
+		const info = await transport.sendMail({
+			from: 'Denis <' + process.env.EMAIL + '>',
+			to: emailAddress,
+			subject: subject,
+			html: template,
+		})
+
+		return !!info
+	},
+}
+
+export const emailAdapter2 = {
 	sendEmail: async (
 		emailAddress: string,
 		subject: string,
